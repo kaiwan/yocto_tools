7bad53130ff9f28a3586184d1f432aee682eb2f8
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2026-06-08 07:43:40 +0530

    comments; simpler implementation
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- lookup_recipe.sh
+++ lookup_recipe.sh
@@ -2,6 +2,11 @@
 # Part of the 'yocto_tools' GitHub repo
 # 'A small, simple and useful tool collection for Yocto'
 #  https://github.com/kaiwan/yocto_tools
+#
+# This tool is essentially a wrapper over the 'oe-pkgdata-util lookup-recipe' ;
+# it's VERY time-consuming as it iterates over each IMAGE built and within it,
+# each package, invoking - oe-pkgdata-util lookup-recipe <pkg> - on each package!
+#
 # (c) Kaiwan NB, kaiwanTECH
 # License: MIT
 export name=$(basename "$0")
@@ -9,27 +14,36 @@ PFX=$(dirname "$(which "$0")")    # dir in which the common code and tools resid
 source "${PFX}"/common || {
   echo "${name}: could not source 'common' script, aborting..." ; exit 1
 }
-setup_env #-q
 
-IMAGE_BASENAME=$(${PFX}/showvars2 IMAGE_BASENAME |tail -n1 |cut -d: -f2)
-[[ -z "${IMAGE_BASENAME}" ]] && die "couldn't fetch value of IMAGE_BASENAME"
-
-MACHINE=$(${PFX}/showvars2 MACHINE |grep "^MACHINE" |cut -d: -f2|tail -n1)
-[[ -z "${MACHINE}" ]] && die "couldn't fetch value of MACHINE"
-
-IMAGE_MACH_DIR=$(\ls -1t -d ${BUILDDIR}/tmp/deploy/licenses/${IMAGE_BASENAME}-${MACHINE}-* |head -n1)
-[[ -z "${IMAGE_MACH_DIR}" ]] && die "couldn't fetch value of the image-mach dir"
-PKG_MANIFEST=${IMAGE_MACH_DIR}/package.manifest
-[ ! -f ${PKG_MANIFEST} ] && die "Oops, the package manifest file \"${PKG_MANIFEST}\" isn't found"
-
+display_recipe_dtl()
+{
+PKG_MANIFEST=${1}/package.manifest
+[ ! -f ${PKG_MANIFEST} ] && {
+  echo "Oops, the package manifest file \"${PKG_MANIFEST}\" isn't found"
+  return
+}
 numpkg=$(wc -l ${PKG_MANIFEST} |cut -d" " -f1)
 [ ${numpkg} -le 0 ] && die "Oops, you have 0 packages generated"
 
 echo "Looking up recipes for the ${numpkg} packages currently generated:
-pkg-name: recipe-to-generate-it
+package-name            : recipe-to-generate-it
 "
 for pkg in $(cat ${PKG_MANIFEST})
 do
-  recp=$(oe-pkgdata-util lookup-recipe ${pkg})
+  recp=$(oe-pkgdata-util lookup-recipe ${pkg} || true)
   printf "%-40s: %s\n" ${pkg} ${recp}
 done
+}
+
+
+#--- 'main'
+setup_env #-q
+
+echo
+for IMAGE_DIR in $(ls -d ${BUILDDIR}/tmp/deploy/licenses/${MACHINE}/*.rootfs)
+do
+ echo "
+----------- IMAGE_DIR = ${IMAGE_DIR} -----------"
+ display_recipe_dtl ${IMAGE_DIR}
+done
+exit 0
